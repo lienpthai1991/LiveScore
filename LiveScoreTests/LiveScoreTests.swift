abdb6c7c5a3336665e94f4f0f6524e587de695e6
Thai Lien <thailien@thais-macbook-pro.local>
2018-06-02 12:01:16 +0700

    Initial Commit

---
+++ LiveScoreTests/LiveScoreTests.swift
@@ -0,0 +1,36 @@
+//
+//  LiveScoreTests.swift
+//  LiveScoreTests
+//
+//  Created by Thai Lien on 6/2/18.
+//  Copyright © 2018 Thai Lien. All rights reserved.
+//
+
+import XCTest
+@testable import LiveScore
+
+class LiveScoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
